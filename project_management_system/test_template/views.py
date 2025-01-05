a8bc295160627cf296422ecf476e762aa26e310a
centaurusgod <ozonewagle998@gmail.com>
2025-01-05 12:56:03 +0545

    Update Content On Test Template

--- project_management_system/test_template/views.py
+++ project_management_system/test_template/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse, request
 
 def index(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return HttpResponse('<h1>Hello World Tilak and ozone</h1>')
